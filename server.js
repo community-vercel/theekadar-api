09fa21ed85f131f384b8544c16793b9df63e38bd
community-vercel <communityvercel@gmail.com>
2025-07-25 05:38:13 -0700

    aaa

--- server.js
+++ server.js
@@ -110,6 +110,7 @@ app.use((req, res) => {
   res.status(404).json({ error: 'Route not found' });
 });
 
+
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 process.on('SIGTERM', () => {
