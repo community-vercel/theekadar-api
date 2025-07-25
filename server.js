64b58ee9be6d7895f3476092e3acad087c8a1e08
community-vercel <communityvercel@gmail.com>
2025-07-25 05:33:02 -0700

    aaa

--- server.js
+++ server.js
@@ -116,8 +116,6 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// Graceful shutdown
 process.on('SIGTERM', () => {
   console.log('SIGTERM received. Shutting down gracefully...');
   server.close(() => {
