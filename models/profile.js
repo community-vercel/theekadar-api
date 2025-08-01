8598e58762e1af6a3c3d14500ad434379947cc7d
community-vercel <communityvercel@gmail.com>
2025-08-01 22:17:19 +0500

    aaa

--- models/profile.js
+++ models/profile.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 
 const profileSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  name: { type: String, required: true },
-  phone: { type: String },
+  name: { type: String, required: false },
+  phone: { type: String,required: false },
   address: { type: String },
   location: {
     type: { type: String, enum: ['Point'], default: 'Point' },
