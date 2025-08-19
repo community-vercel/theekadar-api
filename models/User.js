1d75ffc9b50414292f5e976e566ee029aa03b2fe
community-vercel <communityvercel@gmail.com>
2025-08-19 19:41:41 +0500

    aaa

--- models/User.js
+++ models/User.js
@@ -27,7 +27,6 @@ const userSchema = new mongoose.Schema({
   role: { 
     type: String, 
     enum: ['client', 'worker', 'thekadar', 'contractor', 'consultant', 'admin'], 
-    required: true 
   },
   profileImage: { type: String },
   isVerified: { type: Boolean, default: false },
