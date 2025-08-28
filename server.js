3e1a1c99dcb281b61e2600b73a73cdb9c513f60d
community-vercel <communityvercel@gmail.com>
2025-08-28 23:35:16 +0500

    aa

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.set('trust proxy', 1);
 app.use(
   rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 1000,
+    max: 1000  ,
     standardHeaders: true,
     legacyHeaders: false,
   })
